87119a48eb06b8b5271b54a47bec6acc68565310
XimeCraft <santochaoya@yahoo.com.hk>
2022-09-10 23:46:03 +0800

    colored word

--- test.py
+++ test.py
@@ -9,10 +9,6 @@ ws = Tk()
 ws.title('Check Cantonese')
 center_window(ws)
 
-RED = "\033[31m"
-GREEN = "\033[32m"
-RESET = "\033[39m"
-
 lyric_dir = 'data/songs/寻找独角兽.txt'
 clean_l = lc.read_lyrics(lyric_dir)
 cn_l, en_l = clean_l[::2], lc.split_words_tones(clean_l[1::2])
@@ -34,7 +30,10 @@ b2 = Button(ws, text='Next', command=lambda: next_lyrics(display_c_l, cn_l, t, e
 l.grid(column=0, row=0, columnspan=2, padx=10, pady=20)
 e.grid(column=0, row=1, columnspan=2, pady=10)
 e.focus_set()
+
 t.grid(column=0, row=2, columnspan=2, pady=10)
+t.tag_config('warning', foreground='green')
+
 b1.grid(column=0, row=3, padx=10, pady=10)
 b2.grid(column=1, row=3, padx=10, pady=10)
 
@@ -48,6 +47,7 @@ def next_lyrics(display_c_l, cn_l, t, e):
 
     t.delete("1.0", "end")
     e.delete(0, 'end')
+    e.focus_set()
 
 def check_lyrics(input_l, cn_l, en_l, t):
     global N
@@ -59,13 +59,21 @@ def check_lyrics(input_l, cn_l, en_l, t):
 
     for w in range(len(check_words)):
         if (check_words[w] != en_l[w]) and ('/' not in en_l[w] or check_words[w] not in en_l[w].split('/')):
-            check_words[w] = RED + check_words[w] + RESET
-            colored_en_l[w] = GREEN + en_l[w] + RESET
+            t.insert('insert', en_l[w], 'warning')
+            t.insert('insert', '  ')
+        else:
+            t.insert('insert', f'{en_l[w]}  ')
 
-    output_ls = ' '.join(check_words)    
-    correct_ls = ' '.join(colored_en_l)  
+            # check_words[w] = RED + check_words[w] + RESET
+            # colored_en_l[w] = GREEN + en_l[w] + RESET
 
-    t.delete("1.0", "end")
-    t.insert('insert', correct_ls)
+    # output_ls = ' '.join(check_words)    
+    # correct_ls = ' '.join(colored_en_l)  
+
+    # e.delete(0, END)
+    # e.insert(0, output_ls)
+
+
+
 
 ws.mainloop()
